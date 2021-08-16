477e8fe7b0a05f402bb74b8e64baa4f04a650263
amirrzw <amirrezaabotalebi81@gmail.com>
2021-08-16 16:20:55 +0430

    Initial commit

---
+++ articles/models.py
@@ -0,0 +1,15 @@
+from django.db import models
+
+
+class Article(models.Model):
+    title = models.CharField(max_length=100)
+    slug = models.SlugField(max_length=100)
+    body = models.TextField()
+    date = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.title
+
+    def short_view(self):
+        return f"{self.body[:70]} ..."
+
