293fce8bc17fe79b6d9edb0f561cdbf38bef46c7
amirrzw <amirrezaabotalebi81@gmail.com>
2021-08-17 19:18:32 +0430

    article-image update

--- djangoProject/urls.py
+++ djangoProject/urls.py
@@ -16,6 +16,9 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 from djangoProject import views
+from django.contrib.staticfiles.urls import staticfiles_urlpatterns
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -23,3 +26,6 @@ urlpatterns = [
     path('', views.home),
     path('article/', include('articles.urls')),
 ]
+
+urlpatterns += staticfiles_urlpatterns()
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
