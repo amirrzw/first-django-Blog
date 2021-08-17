651d10dbbd95dfeead3da7a5d1ed5c75a753ed79
amirrzw <amirrezaabotalebi81@gmail.com>
2021-08-17 17:39:27 +0430

    article detail update

--- articles/views.py
+++ articles/views.py
@@ -4,9 +4,11 @@ from . import models
 
 # Create your views here.
 def articles_list(request):
-    articles = models.Article.objects.all().order_by('date')
+    articles = models.Article.objects.all().order_by('-date')
     args = {'articles': articles}
     return render(request, 'articlesList.html', args)
 
+
 def article_detail(request, slug):
-    return HttpResponse(slug + " kiram dahanet")
+    article = models.Article.objects.get(slug=slug)
+    return render(request, 'article_details.html', {'article': article})
