e8e6d35618b4368251c241eef33406c8cc3d505b
amirrzw <amirrezaabotalebi81@gmail.com>
2021-08-19 15:43:12 +0430

    logout

--- users/urls.py
+++ users/urls.py
@@ -4,5 +4,6 @@ from . import views
 app_name = 'users'
 urlpatterns = [
     path('signup', views.signup, name='signup'),
-    path('login', views.login, name='login'),
+    path('login', views.login_view, name='login'),
+    path('logout', views.logout_view, name='logout')
 ]
